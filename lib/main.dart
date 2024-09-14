4b748fa4eff7807124d9b907f189b6f132548680
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-09-14 15:09:29 +0700

    feat: connect to firebase ft ios confugration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:webinar_x/screens/home_screen.dart';
 import 'package:webinar_x/screens/login_screen.dart';
 import 'package:webinar_x/themes/colors.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const WebinarX());
 }
 
